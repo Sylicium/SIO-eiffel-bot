067f38ee1ee6d0f58cf3aad3e472e7f0d779b838
Sylicium <96630108+Sylicium@users.noreply.github.com>
2022-09-17 01:56:39 +0200

    Fix

--- config.js
+++ config.js
@@ -13,7 +13,7 @@ module.exports = {
         prefix: "!",
         id: "1014189091249733722",
         token: process.env["TOKEN"],
-        setApplicationCommandsOnStart: true,
+        setApplicationCommandsOnStart: false,
         setApplicationCommandsInLocal: true,
         setApplicationCommandsInLocal_guilds: [
             "792139282831507467", // test de bots
